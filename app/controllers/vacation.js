39d74f6df27f9d031f27bad337e4de5a77487aef
sirgalleto <sebastian.osorio@wizeline.com>
2017-05-02 17:53:23 -0500

    Create base object for vacations

---
+++ app/controllers/vacation.js
@@ -0,0 +1,17 @@
+export default class VacationController {
+  static list(req, res) {
+    res.json({});
+  }
+  static find(req, res) {
+    res.json({});
+  }
+  static create(req, res) {
+    res.json({});
+  }
+  static update(req, res) {
+    res.json({});
+  }
+  static delete(req, res) {
+    res.json({});
+  }
+}
