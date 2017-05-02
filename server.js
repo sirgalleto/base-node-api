be0952311ac75566db214489b20badf02060a714
sirgalleto <sebastian.osorio@wizeline.com>
2017-05-01 20:40:30 -0500

    Config express

---
+++ server.js
@@ -0,0 +1,25 @@
+import config from 'config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const apiConfig = config.get('api');
+const app = express();
+
+app.set('case sensitive routing', true);
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Content-Type', 'application/json');
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  next();
+});
+
+app.listen(apiConfig.port, apiConfig.host);
